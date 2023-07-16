a27723f3d20779bcd3ca15e1b08e9a8b3f4c5a7e
India Cornell <indiaccornell@gmail.com>
2023-07-15 21:10:06 -0700

    update for name and connection notification

--- client.js
+++ client.js
@@ -8,6 +8,10 @@ const connect = function () {
 
   // interpret incoming data as text
   conn.setEncoding("utf8");
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server");
+    conn.write("Name: SAM");
+  });
   conn.on("data", (data) => {
     console.log("Server says: ", data);
   });
