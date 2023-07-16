729befbfb76cbd8e9e90ce37e010856adc6fcd8c
India Cornell <indiaccornell@gmail.com>
2023-07-15 21:15:05 -0700

    add and comment out move: up message

--- client.js
+++ client.js
@@ -11,6 +11,10 @@ const connect = function () {
   conn.on("connect", () => {
     console.log("Successfully connected to game server");
     conn.write("Name: SAM");
+    //conn.write("Move: up");
+    //setInterval(() => {
+    //   conn.write("Move: up");
+    // }, 50);
   });
   conn.on("data", (data) => {
     console.log("Server says: ", data);
