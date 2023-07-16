db695fec01d64b2701234c9006dbaac9e938652a
India Cornell <indiaccornell@gmail.com>
2023-07-15 22:10:34 -0700

    update snake controls

--- play.js
+++ play.js
@@ -1,20 +1,7 @@
 const { connect } = require("./client.js");
+const { setupInput } = require("./input.js");
+
 console.log("Connecting ...");
-connect();
 
-// setup interface to handle user input from stdin
-
-const setupInput = function () {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
-const handleUserInput = function (key) {
-  if (key === "\u0003") {
-    process.exit();
-  }
-};  
-setupInput();
+const conn = connect();
+setupInput(conn);
